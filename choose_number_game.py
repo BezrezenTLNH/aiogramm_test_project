be8036fd8385de6b668bb86693d500cc84f09a4f
Mikhail Zherebtsov <bezrezen97@gmail.com>
2024-01-19 22:39:32 +0100

    finished guess number bot game

--- choose_number_game.py
+++ choose_number_game.py
@@ -17,12 +17,8 @@ dp = Dispatcher()
 # Qty of tries that user can use
 ATTEMPTS = 5
 
-# Dictionary that contains user data without DB
-user = {'in_game': False,
-        'secret_number': None,
-        'attempts': None,
-        'total_games': 0,
-        'wins': 0}
+# Dictionary that contains users and their data without DB
+users = {}
 
 
 # Function that create a random number from 1 to 100
@@ -38,6 +34,16 @@ async def process_start_command(message: Message):
         'To get a play rules and the list of a available'
         'commands - send command /help'
     )
+    # If user used the bot at the first time and this user not in 'users' dictionary '
+    # 'users - we will add him to this dict
+    if message.from_user.id not in users:
+        users[message.from_user.id] = {
+            'in_game': False,
+            'secret_number': None,
+            'attempts': None,
+            'total_games': 0,
+            'wins': 0
+        }
 
 
 # This handler will trigger on command "/help"
@@ -52,19 +58,19 @@ async def process_help_command(message: Message):
 
 
 # This handler will trigger on command "/start"
-@dp.message(Command(commands='start'))
+@dp.message(Command(commands='stat'))
 async def process_start_command(message: Message):
     await message.answer(
-        f"Total played games: {user['total_games']}\n"
-        f"Won games: {user['wins']}"
+        f"Total played games: {users[message.from_user.id]['total_games']}\n"
+        f"Won games: {users[message.from_user.id]['wins']}"
     )
 
 
 # This handler will trigger on command "/cancel"
 @dp.message(Command(commands='cancel'))
 async def process_cancel_command(message: Message):
-    if user['in_game']:
-        user['in_game'] = False
+    if users[message.from_user.id]['in_game']:
+        users[message.from_user.id]['in_game'] = False
         await message.answer(
             "You're leave the game. If you want to play "
             "again - text me about it"
@@ -80,10 +86,10 @@ async def process_cancel_command(message: Message):
 @dp.message(F.text.lower().in_(['yes', 'ye', 'lets play', 'play',
                                 'game', 'want to play']))
 async def process_positive_answer(message: Message):
-    if not user['in_game']:
-        user['in_game'] = True
-        user['secret_number'] = get_random_number()
-        user['attempts'] = ATTEMPTS
+    if not users[message.from_user.id]['in_game']:
+        users[message.from_user.id]['in_game'] = True
+        users[message.from_user.id]['secret_number'] = get_random_number()
+        users[message.from_user.id]['attempts'] = ATTEMPTS
         await message.answer(
             'Hurray!\n\nI wished a number rom 1 to 100, '
             'try to guess!'
@@ -99,7 +105,7 @@ async def process_positive_answer(message: Message):
 # This handler will trigger if the user refuses to play the game
 @dp.message(F.text.lower().in_(['no', 'not', 'nope', "don't want"]))
 async def process_negative_answer(message: Message):
-    if not user['in_game']:
+    if not users[message.from_user.id]['in_game']:
         await message.answer(
             "It's a pity :(\n\nIf you want to play, just "
             "write about it"
@@ -114,29 +120,30 @@ async def process_negative_answer(message: Message):
 # This handler will trigger on messages with number from 1 to 100 that user will send
 @dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 100)
 async def process_numbers_answer(message: Message):
-    if user['in_game']:
-        if int(message.text) == user['secret_number']:
-            user['in_game'] = False
-            user['total_games'] += 1
-            user['wins'] += 1
+    print(users)
+    if users[message.from_user.id]['in_game']:
+        if int(message.text) == users[message.from_user.id]['secret_number']:
+            users[message.from_user.id]['in_game'] = False
+            users[message.from_user.id]['total_games'] += 1
+            users[message.from_user.id]['wins'] += 1
             await message.answer(
                 "Yay!!! You're right!\n\n"
                 "Let's play again?"
             )
-        elif int(message.text) > user['secret_number']:
-            user['attempts'] -= 1
+        elif int(message.text) > users[message.from_user.id]['secret_number']:
+            users[message.from_user.id]['attempts'] -= 1
             await message.answer('My number is lower')
-        elif int(message.text) < user['secret_number']:
-            user['attempts'] -= 1
+        elif int(message.text) < users[message.from_user.id]['secret_number']:
+            users[message.from_user.id]['attempts'] -= 1
             await message.answer('My number is bigger')
 
-        if user['attempts'] == 0:
-            user['in_game'] = False
-            user['total_games'] += 1
+        if users[message.from_user.id]['attempts'] == 0:
+            users[message.from_user.id]['in_game'] = False
+            users[message.from_user.id]['total_games'] += 1
             await message.answer(
                 f"Unfortunately, you have no more "
                 f"attempts. You lost :(\n\nMy number "
-                f"was {user['secret_number']}\n\nLet's "
+                f"was {users[message.from_user.id]['secret_number']}\n\nLet's "
                 f'play again?'
             )
     else:
@@ -146,7 +153,7 @@ async def process_numbers_answer(message: Message):
 # This handler will trigger by other messages
 @dp.message()
 async def process_other_answers(message: Message):
-    if user['in_game']:
+    if users[message.from_user.id]['in_game']:
         await message.answer(
             "We're playing with you now. "
             "Please send numbers from 1 to 100"
---
+++ filters_practice.py
@@ -0,0 +1,90 @@
+import os
+
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import BaseFilter
+from aiogram.types import Message
+from aiogram.types import PhotoSize
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('TG_TOKEN')
+
+# Create a bot and dispatcher objects
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+# def custom_filter(some_list):
+#     return sum([x for x in some_list if isinstance(x, int) and x % 7 == 0]) <= 83
+
+# anonymous_filter = lambda s: s.lower().count('я') >= 23
+
+# @dp.message(lambda msg: msg.text == '/start')
+# async def process_start_command(message: Message):
+#     await message.answer(text='Это команда /start')
+#
+#
+# if __name__ == '__main__':
+#     dp.run_polling(bot)
+#
+# admin_ids: list[int] = [111111111]
+#
+#
+# class IsAdmin(BaseFilter):
+#     def __init__(self, admin_ids: list[int]) -> None:
+#         self.admin_ids = admin_ids
+#
+#     async def __call__(self, message: Message) -> bool:
+#         return message.from_user.id in self.admin_ids
+#
+#
+# # This handler will trigger if he will catch update from admin
+# @dp.message(IsAdmin(admin_ids))
+# async def answer_if_admins_update(message: Message):
+#     await message.answer(text="You're admin")
+#
+#
+# # This handler will trigger if he will catch update not from admin
+# @dp.message()
+# async def answer_if_not_admins_update(message: Message):
+#     await message.answer(text="You're not admin")
+
+
+# This handler will check negative numbers in user's messages and make a list from them
+class NumbersInMessage(BaseFilter):
+    async def __call__(self, message: Message) -> bool | dict[str, list[int]]:
+        numbers = []
+        for word in message.text.split():
+            normalized_word = word.replace('.', '').replace(',', '').strip()
+            if normalized_word.isdigit():
+                numbers.append(int(normalized_word))
+        if numbers:
+            return {'numbers': numbers}
+        return False
+
+
+# This handler will trigger if the user's message
+# starts with the phrase "find the numbers" and has numbers in it
+@dp.message(F.text.lower().startswith('find the numbers'), NumbersInMessage())
+# In addition to the Message type object, we accept a list of numbers from the filter into the handler
+async def process_if_numbers(message: Message, numbers: list[int]):
+    await message.answer(
+        text=f'Found it: {", ".join(str(num) for num in numbers)}')
+
+
+# This handler will trigger if the user's message
+# starts with the phrase "find the numbers", but there are no numbers in it
+@dp.message(F.text.lower().startswith('find the numbers'))
+async def process_if_not_numbers(message: Message):
+    await message.answer(
+        text="Not found:(")
+
+
+@dp.message(F.photo[0].as_('photo_min'))
+async def process_photo_send(message: Message, photo_min: PhotoSize):
+    print(photo_min)
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
